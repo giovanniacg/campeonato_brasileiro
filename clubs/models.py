24357b938b0f7a09cc409ed5a4af2060839894f8
Giovanni Giampauli <giovanni.acg@gmail.com>
2025-10-21 16:42:38 -0300

    GREEN: Cria model para resolver o teste

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=100)

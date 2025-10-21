8408cdf5e57105f7c61e1c8c9d4fcbc9c15497be
Giovanni Giampauli <giovanni.acg@gmail.com>
2025-10-21 19:11:14 -0300

    GREEN: Cria a model leagueseason para passar nos testes

--- leagues/models.py
+++ leagues/models.py
@@ -1,3 +1,13 @@
+from core.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class LeagueSeason(BaseModel):
+    year = models.PositiveIntegerField(unique=True)
+    parent_league = models.OneToOneField(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="subseason",
+    )
--- leagues/tests/factories.py
+++ leagues/tests/factories.py
@@ -1,7 +1,7 @@
 from factory.django import DjangoModelFactory
 from factory.faker import Faker
 from factory.declarations import SubFactory
-from clubs.models import LeagueSeason
+from leagues.models import LeagueSeason
 
 
 class LeagueSeasonFactory(DjangoModelFactory):

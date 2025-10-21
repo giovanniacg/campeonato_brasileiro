f6096ada267ff25d6d644f6140929b8e6d7baab4
Giovanni Giampauli <giovanni.acg@gmail.com>
2025-10-21 16:49:32 -0300

    REFACTOR: Cria app core para criar um BaseModel e herdar em time

--- clubs/models.py
+++ clubs/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from core.models import BaseModel
 
 
-class Team(models.Model):
+class Team(BaseModel):
     name = models.CharField(max_length=100)
     short_name = models.CharField(max_length=50)
     city = models.CharField(max_length=100)

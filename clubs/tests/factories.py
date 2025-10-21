42ef447d2c6fa79cb6755850c6234756905e8387
Giovanni Giampauli <giovanni.acg@gmail.com>
2025-10-21 16:59:55 -0300

    RED: cria testes para divisao de times

--- clubs/tests/factories.py
+++ clubs/tests/factories.py
@@ -1,6 +1,7 @@
 from factory.django import DjangoModelFactory
 from factory.faker import Faker
 from clubs.models import Team
+from clubs.models import LeagueDivision
 
 
 class TeamFactory(DjangoModelFactory):
@@ -10,3 +11,11 @@ class TeamFactory(DjangoModelFactory):
     name = Faker("name", locale="pt_BR")
     short_name = Faker("word", locale="pt_BR")
     city = Faker("city", locale="pt_BR")
+
+
+class LeagueDivisionFactory(DjangoModelFactory):
+    class Meta:
+        model = LeagueDivision
+
+    name = Faker("word", locale="pt_BR")
+    parent_league = None
---
+++ clubs/tests/test_league_division_model.py
@@ -0,0 +1,15 @@
+import pytest
+from .factories import LeagueDivisionFactory
+
+
+@pytest.mark.django_db
+def test_create_league_division():
+    league_division = LeagueDivisionFactory()
+    assert league_division.pk is not None
+
+
+@pytest.mark.django_db
+def test_league_division_parent_league():
+    parent_league = LeagueDivisionFactory()
+    child_league = LeagueDivisionFactory(parent_league=parent_league)
+    assert child_league.parent_league == parent_league

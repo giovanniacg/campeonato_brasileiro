7e1af3065cae239ad98b74be9b1996ff2e556e74
Giovanni Giampauli <giovanni.acg@gmail.com>
2025-10-27 14:31:59 -0300

    REFACTOR: adiciona uuid nos id das models

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 
 class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid7, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
